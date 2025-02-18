9f92f2129fcc92aebb35ab2076a79672aca0f8bf
Justify-social <ed@justify.social>
2025-02-18 21:42:44 +0000

    fix: use relative paths and add brand lift components

--- src/app/brand-lift/survey-preview/page.tsx
+++ src/app/brand-lift/survey-preview/page.tsx
@@ -4,7 +4,7 @@ import React, { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 
 const SurveyPreviewContent = dynamic(
-  () => import('@/components/brand-lift/SurveyPreviewContent'),
+  () => import('../../../components/brand-lift/SurveyPreviewContent'),
   {
     ssr: false,
     loading: () => <div>Loading...</div>
