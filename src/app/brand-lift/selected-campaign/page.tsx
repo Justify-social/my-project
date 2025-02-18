31a4dec23596319323b3ea5c0e77c384eb31f5f9
Justify-social <ed@justify.social>
2025-02-18 21:39:05 +0000

    add: brand lift components and pages with correct imports

--- src/app/brand-lift/selected-campaign/page.tsx
+++ src/app/brand-lift/selected-campaign/page.tsx
@@ -4,7 +4,7 @@ import React, { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 
 const SelectedCampaignContent = dynamic(
-  () => import('../../../components/brand-lift/SelectedCampaignContent'),
+  () => import('@/components/brand-lift/SelectedCampaignContent'),
   {
     ssr: false,
     loading: () => <div>Loading...</div>
--- src/app/brand-lift/survey-approval/page.tsx
+++ src/app/brand-lift/survey-approval/page.tsx
@@ -4,7 +4,7 @@ import React, { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 
 const SurveyApprovalContent = dynamic(
-  () => import('../../../components/brand-lift/SurveyApprovalContent'),
+  () => import('@/components/brand-lift/SurveyApprovalContent'),
   {
     ssr: false,
     loading: () => <div>Loading...</div>
