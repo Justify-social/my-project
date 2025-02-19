84b1822fb2c9d09db053810f636bc6fc4fc963bd
Justify-social <ed@justify.social>
2025-02-19 09:17:56 +0000

    fix: add webpack alias configuration for @ imports

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': `${__dirname}/src`,
+      '@': require('path').resolve(__dirname, 'src'),
     };
     return config;
   },
