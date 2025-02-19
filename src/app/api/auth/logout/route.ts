7aef048b4c5bad48d2fe6e45f275f090768ab69d
Justify-social <ed@justify.social>
2025-02-19 11:35:04 +0000

    Update logout code with federated logout

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -2,7 +2,10 @@ import { handleLogout } from '@auth0/nextjs-auth0';
 
 export const GET = async (request: Request) => {
   try {
-    return await handleLogout(request, { returnTo: 'https://app.justify.social/' });
+    return await handleLogout(request, { 
+      returnTo: 'https://app.justify.social/', 
+      logoutParams: { federated: true }
+    });
   } catch (error) {
     console.error('Error in logout route:', error);
     return new Response('Internal Server Error', { status: 500 });
