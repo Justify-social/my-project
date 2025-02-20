e96569a03edd3d51606133b6a47e5149f074203d
Justify-social <ed@justify.social>
2025-02-20 23:48:14 +0000

    test: add auth verification pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
-import { redirect } from 'next/navigation';
+"use client";
+
+import { useAuth } from "@clerk/nextjs";
 
 export default function Home() {
-  // Redirect to dashboard by default
-  redirect('/dashboard');
+  const { isLoaded, isSignedIn } = useAuth();
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {isSignedIn ? (
+        <div>You are signed in!</div>
+      ) : (
+        <div>Please sign in!</div>
+      )}
+    </div>
+  );
 }
