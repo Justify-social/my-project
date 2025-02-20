c00f9884262ad43fc83dc8f420a7835d3bfc01bf
Justify-social <ed@justify.social>
2025-02-20 15:33:26 +0000

    fix: update clerk middleware import and sentry config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,18 @@
-import { authMiddleware } from "@clerk/nextjs"
+import { authMiddleware } from "@clerk/nextjs/server"
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks(.*)", "/api/uploadthing"],
+  publicRoutes: [
+    "/",
+    "/api/webhooks(.*)",
+    "/api/uploadthing",
+    "/api/auth/callback"
+  ],
 })
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)"
+  ],
 } 
