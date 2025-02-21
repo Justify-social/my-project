bd0e2fd2d42372d525206c2e3623f6fe930fd32b
Justify-social <ed@justify.social>
2025-02-21 00:31:48 +0000

    fix: wrap dashboard with SidebarProvider

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,8 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import DashboardContent from "@/components/DashboardContent";
-import { Header, Sidebar } from "@/components/Navigation";
+import { Header, Sidebar, MobileMenu } from "@/components/Navigation";
+import { SidebarProvider } from "@/providers/sidebar-provider";
 
 export default async function DashboardPage() {
   const { userId } = auth();
@@ -11,12 +12,15 @@ export default async function DashboardPage() {
   }
 
   return (
-    <div className="flex h-screen bg-gray-100">
-      <Sidebar />
-      <div className="flex-1 overflow-auto">
-        <Header />
-        <DashboardContent />
+    <SidebarProvider>
+      <div className="flex h-screen bg-gray-100">
+        <Sidebar />
+        <div className="flex-1 overflow-auto">
+          <Header />
+          <MobileMenu />
+          <DashboardContent />
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 }
